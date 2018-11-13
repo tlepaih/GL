2e37b0d6e559dbd0a8623272fc7f9c3c590d657f
tlepaih <theo.le-paih@u-psud.fr>
2018-11-13 22:36:10 +0100

    Exo43

--- src/main.c
+++ src/main.c
@@ -2,8 +2,29 @@
 //
 
 #include "my_header.h"
+#include <stdlib.h> 
 
 int main(void)
 {
-    return 0;
+  unsigned char n=1;
+  unsigned short int s1=3;
+  unsigned short int s2=3
+
+//question 1
+  while(n<12)
+  {
+   s1=s1+3n;
+   n=n+1
+
+  }
+//question 2
+  while(s2<9876)
+   {
+    s2=s2+3n;
+    n=n+1;
+   }
+//question 3
+//VSCode ne trouve pas stdlib.h
+ 
+ return 0;
 }
