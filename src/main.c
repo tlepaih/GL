377655d8411a8d9f685b7b732f64f9d6588fe96b
theo le-paih <theo.le-paih@ge1.iut-cachan.u-psud.fr>
2018-11-13 11:27:53 +0100

    Correction exo2 DS

--- src/main.c
+++ src/main.c
@@ -1,6 +1,33 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+// 1) Plage de valeurs type short int
+//    - 32 768 < short int < 32 767     (2 octects)
+
+// 2) Equation du 2nd degré
+
+#include "my_header.h"
+#include <math.h>
+
+#define ECART 1e-10
+
+int main(void)
+{
+    double a = 1.0, b = 2.0, c = 3.0;
+    double delta;
+    unsigned char nb_solutions;
+
+    delta = b*b - 4.0 * a *c;
+
+    if ( fabs(delta) <  ECART )
+        nb_solutions = 1;
+    else if( delta > 0)
+        nb_solutions = 2;
+    else
+        nb_solutions = 0;
+
+    return 0;
+}
 #include "my_header.h"
 
 int main(void)
