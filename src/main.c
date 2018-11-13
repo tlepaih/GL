800460ff8acdeb37e7feeaef88bf740646ad97b7
theo le-paih <theo.le-paih@ge1.iut-cachan.u-psud.fr>
2018-11-13 11:44:32 +0100

    Correction Exo3 DS

--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+// 3) S1(n) = 1*2*3*...*n
+//    S1(n+1) = S1(n) * (n+1)
+
 #include "my_header.h"
 
 int main(void)
 {
+    unsigned long long int S1 = 1;
+    unsigned char n = 2;
+
+    while (n<50)
+    {
+        S1 = S1 * n;
+        n = n + 1;
+    }
+
     return 0;
 }
