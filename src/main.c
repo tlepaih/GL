8e6849ba481e4ec24cf5f22c289a37ddc3b17b34
tlepaih <theo.le-paih@u-psud.fr>
2018-11-14 21:51:55 +0100

    Exo43

--- src/main.c
+++ src/main.c
@@ -6,25 +6,47 @@
 
 int main(void)
 {
-  unsigned char n=1;
-  unsigned short int s1=3;
-  unsigned short int s2=3
+  unsigned char n=2,i=0, h=0, c=1;
+  int a;
+  unsigned int s1=3, s2=3, s3=0, s4=0;
 
 //question 1
-  while(n<12)
-  {
-   s1=s1+3n;
-   n=n+1
+  while(n<12) {
+	  
+    s1=s1+3*n;
+    n=n+1;
+	}
 
-  }
 //question 2
+	
+  n=2;
+	
   while(s2<9876)
    {
-    s2=s2+3n;
+    s2=s2+3*n;
     n=n+1;
    }
+
 //question 3
-//VSCode ne trouve pas stdlib.h
- 
+   
+   while (i<12){
+    
+	a = (rand()%10)+1;
+	s3 = s3 + a;
+	i = i+1;
+   }   
+
+//question 4
+	
+	while (h<20){
+		
+		if (c%3 == 0 || c%7 == 0){
+			s4 = s4 + c;
+			c = c+1;
+			h = h+1;
+		}
+		else	
+			c = c+1;
+	}
  return 0;
 }
