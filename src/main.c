481771916bf30066073737fe00c3ccba2a059d20
tlepaih <theo.le-paih@u-psud.fr>
2018-11-14 22:37:43 +0100

    Exo45

--- src/main.c
+++ src/main.c
@@ -2,8 +2,29 @@
 //
 
 #include "my_header.h"
+#include "TP_lib.h"
 
 int main(void)
 {
+	short int resultat; 
+	unsigned int score = 0;
+	
+	resultat = LancerDe ();
+	
+	while (resultat != 1) {
+		
+		if (resultat==2 || resultat==4 || resultat==6) 
+			score = score + resultat;
+		
+		else if (resultat==3)
+			score = score*2;
+		
+		else
+			score = score - 2;
+		
+		
+		resultat = LancerDe ();
+	}
+	
     return 0;
 }
