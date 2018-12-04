94f790e6fdb59de2ab08e0706e76eec25e660956
theo le-paih <theo.le-paih@ge1.iut-cachan.u-psud.fr>
2018-12-04 09:41:26 +0100

    Exercice 79 fini

--- src/main.c
+++ src/main.c
@@ -1,9 +1,30 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#include "my_header.h"
+#include <userint.h>
+#include <ansi_c.h>
 
 int main(void)
 {
+	char nom[20], prenom[20],phrase[50];
+	unsigned long annee_naissance,annee_actuelle, age;
+	
+	printf("Entrer votre nom:");
+	scanf("%s",nom);
+	
+	printf("Entrer votre prenom:");
+	scanf("%s",prenom);
+	
+	printf("Entrer votre annee de naissance:");
+	scanf("%lu",&annee_naissance);
+	
+	printf("Entrer l'annee actuelle:");
+	scanf("%lu",&annee_actuelle);
+	
+	age = annee_actuelle - annee_naissance;
+	
+	sprintf(phrase,"Je m'appelle %s %s et j'ai %lu ans.",prenom,nom,age); 
+	MessagePopup("Identite de l'utilisateur",phrase);
+	
     return 0;
 }
